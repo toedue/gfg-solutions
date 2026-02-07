ab6f369f2724ffe72dc87d3a5eedd1a2448a2f66
Abdulkadir Abdo <83957140+toedue@users.noreply.github.com>
2026-02-07 21:28:00 +0300

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
+++ Difficulty: Easy/Union of Arrays with Duplicates/union-of-arrays-with-duplicates.py
@@ -1,7 +1,6 @@
 class Solution:    
     def findUnion(self, a, b):
         # code here
-        #abdulkadir
-        ss = set(a+b)
-        count = len(ss)
-        return count
+        
+        return set (a + b)
+        
